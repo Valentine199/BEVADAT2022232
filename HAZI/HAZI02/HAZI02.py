aa3216997e4bcf5e248b48806bfc306afeda6681
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-03-07 16:15:17 +0100

    column_swap, get_array_shape done

---
+++ HAZI/HAZI02/HAZI02.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+# Írj egy olyan fügvényt, ami megfordítja egy 2d array oszlopait
+# Be: [[1,2],[3,4]]
+# Ki: [[2,1],[4,3]]
+# column_swap()
+def column_swap(input: np.array) -> np.array:
+    input = np.roll(input, -1, axis=1)
+    return input
+
+#TODO Készíts egy olyan függvényt ami összehasonlít két array-t és adjon vissza egy array-ben, hogy hol egyenlőek
+# Pl Be: [7,8,9], [9,8,7]
+# Ki: [1]
+# compare_two_array()
+# egyenlő elemszámúakra kell csak hogy működjön
+
+#Készíts egy olyan függvényt, ami vissza adja a megadott array dimenzióit:
+# Be: [[1,2,3], [4,5,6]]
+# Ki: "sor: 2, oszlop: 3, melyseg: 1"
+# get_array_shape()
+# 3D-vel még műküdnie kell!
+def get_array_shape(array1: np.array) -> str:
+    size = array1.shape
+    return f"sor: {array1.shape[-2] if len(size) >= 2 else 1}, oszlop: {size[-1]}, melyseg: {array1.shape[-3] if len(size) >= 3 else 1}"
+
+# Készíts egy olyan függvényt, aminek segítségével elő tudod állítani egy neurális hálózat tanításához szükséges Y-okat egy numpy array-ből.
+#Bementként add meg az array-t, illetve hogy mennyi class-od van. Kimenetként pedig adjon vissza egy 2d array-t, ahol a sorok az egyes elemek. Minden nullákkal teli legyen és csak ott álljon egyes, ahol a bementi tömb megjelöli
+# Be: [1, 2, 0, 3], 4
+# Ki: [[0,1,0,0], [0, 0, 1, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
+# encode_Y()
