13047f1dda92b0da80c836fdd5162b2ce58e84ad
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2023-04-21 15:07:02 +0200

    HAZI08 fix

--- HAZI/HAZI08/HAZI08.py
+++ HAZI/HAZI08/HAZI08.py
@@ -22,7 +22,7 @@ def load_iris_data() -> sklearn.utils.Bunch:
     return load_iris()
 
 
-iris = load_iris_data()
+#iris = load_iris_data()
 '''
 Készíts egy függvényt, ami a betölti az virágokhoz tartozó levél méretket egy dataframebe, majd az első 5 sort visszaadja.
 Minden oszlop tartalmazza, hogy az milyen mérethez tartozik.
@@ -55,7 +55,7 @@ def linear_train_data(iris):
     y = df["sepal length (cm)"]
     return X, y
 
-linX, linY = linear_train_data(iris)
+#linX, linY = linear_train_data(iris)
 
 ''' 
 Készíts egy függvényt ami előkészíti az adatokat egy logisztikus regressziós model feltanításához.
