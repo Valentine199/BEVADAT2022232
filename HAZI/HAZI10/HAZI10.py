47922d006b400ed065a7779208885205c6f3f12f
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-05-06 12:43:15 +0200

    HAZI10 fix

--- HAZI/HAZI10/HAZI10.py
+++ HAZI/HAZI10/HAZI10.py
@@ -12,11 +12,11 @@ függvény neve: mnist_digit_data
 
 def mnist_digit_data():
   (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-  x_train = x_train.astype("float32") / 255
-  x_test = x_test.astype("float32") / 255
+  x_train = x_train.astype("float32") / 255.0
+  x_test = x_test.astype("float32") / 255.0
 
-  y_train = y_train.astype("float32") / 255
-  y_test = y_test.astype("float32") / 255
+  #y_train = y_train.astype("float32") / 255.0
+  #y_test = y_test.astype("float32") / 255.0
 
   return x_train, y_train, x_test, y_test
 
